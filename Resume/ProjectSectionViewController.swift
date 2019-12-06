1035ce7a0a78582b9ea8de56c01e044e429d9218
Joseph Buchoff <joebuchoff@gmail.com>
2019-12-06 01:24:12 -0600

    Initial Commit

---
+++ Resume/ProjectSectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProjectSectionViewController.swift
+//  Resume
+//
+//  Created by Joseph Buchoff on 12/6/19.
+//  Copyright © 2019 The Spot of Joe. All rights reserved.
+//
+
+import UIKit
+
+class ProjectSectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Resume/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Resume
+//
+//  Created by Joseph Buchoff on 12/4/19.
+//  Copyright © 2019 The Spot of Joe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
